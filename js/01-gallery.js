a06ad996b34671f03713af8ff83c14ba24e55fca
Karol Kotański <karolfm12@onet.eu>
2024-01-03 14:39:24 +0100

    basic2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -10,3 +10,8 @@ const liGallery = galleryItems
   .join("");
 
 ulGallery.innerHTML = liGallery;
+const instance = basicLightbox.create(`
+    <h1>Dynamic Content</h1>
+    <p>You can set the content of the lightbox with JS.</p>
+`);
+console.log(instance.visible);
