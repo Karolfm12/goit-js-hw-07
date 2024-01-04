cf4134b64d75de2da2a38cd42fe56a4e36440978
Karol Kotański <karolfm12@onet.eu>
2024-01-04 14:47:30 +0100

    Task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+// import SimpleLightbox from "../node_modules/simplelightbox/dist/simple-lightbox.min.js";
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
+const ulGallery = document.querySelector(".gallery");
+const liGallery = galleryItems
+  .map(
+    (image) =>
+      `<li class="gallery__item"><a class="gallery__link" href="${image.original}"><img class="gallery__image" src="${image.preview}" data-source="${image.original}" alt="${image.description}"/></a></li>`
+  )
+  .join("");
+ulGallery.innerHTML = liGallery;
+// console.log(galleryItems);
+const modalImages = document.querySelectorAll(".gallery__link");
+for (const modalImage of modalImages) {
+  modalImage.addEventListener("click", (e) => {
+    e.preventDefault();
+    const lightBox = new SimpleLightbox(modalImage);
+    lightBox.show();
+    console.log(lightBox);
+  });
+}
