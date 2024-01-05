a6aedff76143ed6a321c8b1fe4f069bcec2dde97
Karol Kotański <karolfm12@onet.eu>
2024-01-05 10:03:41 +0100

    done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,21 +1,14 @@
-// import SimpleLightbox from "../node_modules/simplelightbox/dist/simple-lightbox.min.js";
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const ulGallery = document.querySelector(".gallery");
 const liGallery = galleryItems
   .map(
     (image) =>
-      `<li class="gallery__item"><a class="gallery__link" href="${image.original}"><img class="gallery__image" src="${image.preview}" data-source="${image.original}" alt="${image.description}"/></a></li>`
+      `<li><a class="gallery__item" href="${image.original}"><img class="gallery__image" src="${image.preview}" alt="${image.description}"/></a></li>`
   )
   .join("");
 ulGallery.innerHTML = liGallery;
-// console.log(galleryItems);
-const modalImages = document.querySelectorAll(".gallery__link");
-for (const modalImage of modalImages) {
-  modalImage.addEventListener("click", (e) => {
-    e.preventDefault();
-    const lightBox = new SimpleLightbox(modalImage);
-    lightBox.show();
-    console.log(lightBox);
-  });
-}
+
+const lightBox = new SimpleLightbox(".gallery li a", {
+  /* */
+});
